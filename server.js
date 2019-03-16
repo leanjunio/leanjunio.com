e31bf986e931108a7bba451b211a2472ea22c597
Lean Junio <leanjunio@live.com>
2019-03-16 18:27:52 -0400

    first comit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const exphbs  = require('express-handlebars');
+
+const routes = require('./routes/index')
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.use(express.static('public'));
+/**
+ * Front-End Routes
+ */
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
