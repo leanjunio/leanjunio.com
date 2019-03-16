b2308dedfc5037f65674048da9457e478445331c
Lean Junio <leanjunio@live.com>
2019-03-16 18:39:12 -0400

    update routes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const exphbs  = require('express-handlebars');
 
-const routes = require('./routes/index')
+// const routes = require('./routes/index')
 const app = express();
 
 const PORT = process.env.PORT || 5000;
